ae40560e72a7f71e1ecf736de19f19da6eb2d13d
Hitsam Tiammar <hitsam@landx.id>
2021-10-18 00:43:10 +0700

    app

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,8 @@
 </template>
 
 <script>
-const GOOGLE_MAPS_API_KEY = "AIzaSyCJfNSlTfsEGFo5FrYLHXzQzRdFtLtY9Oo";
 export default {
   name: "App",
-
-  created() {
-    const checkScript = document.getElementById("google-maps");
-    if (!checkScript) {
-      const script = document.createElement("script");
-      const src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}`;
-      script.setAttribute("id", "google-maps");
-      script.setAttribute("async", "");
-      script.setAttribute("src", src);
-      document.head.appendChild(script);
-    }
-  },
 
   data: () => ({
     //
